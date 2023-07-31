77f4c8266c26a8a7f88ff42f6af6c7af512db8ea
Taki Shiwa <judhistiram@protonmail.com>
2023-07-31 22:11:46 +0530

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -2,5 +2,5 @@
 
 directory_to_count="Button/SVG"
 
-file_count=$(find Button/SVG -type f | wc -l)
+file_count=$(find "$Button/SVG" -type f | wc -l)
 echo "{\"schemaVersion\": 1, \"label\": \"Files\", \"message\": \"$file_count\", \"color\": \"blue\"}"
