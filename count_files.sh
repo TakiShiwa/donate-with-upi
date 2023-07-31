58414eea0f4d318d2cc117cc0414e404dc388871
Taki Shiwa <judhistiram@protonmail.com>
2023-07-31 22:45:11 +0530

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -2,5 +2,5 @@
 
 directory_to_count="Button/SVG"
 
-message=$(find "$directory_to_count" -type f | wc -l)
-printf "{\"schemaVersion\": 1, \"label\": \"Files\", \"message\": \"$message\", \"color\": \"blue\"}"
+file_count=$(find "$directory_to_count" -type f | wc -l)
+printf "{\"schemaVersion\": 1, \"label\": \"Files\", \"message\": \"$file_count\", \"color\": \"blue\"}"
