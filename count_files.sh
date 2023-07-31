6bccd475938a3e7a419f4dc9b7b1abc0fe0a5dac
Taki Shiwa <judhistiram@protonmail.com>
2023-07-31 23:28:43 +0530

    Delete count_files.sh

--- count_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-directory_to_count="Button/SVG"
-
-file_count=$(find "$directory_to_count" -type f | wc -l)
-printf "{\"schemaVersion\": 1, \"label\": \"Files\", \"message\": \"$file_count\", \"color\": \"blue\"}"
