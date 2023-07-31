938bdfd1b9ecc24422587bf3c381b60a1b5ea749
Taki Shiwa <judhistiram@protonmail.com>
2023-07-31 10:06:51 +0530

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change the directory path to the target directory
-DIRECTORY_PATH="Button/PNG"
+DIRECTORY_PATH="Button/SVG"
 
 # Count the number of files in the target directory
 FILE_COUNT=$(find "$DIRECTORY_PATH" -type f | wc -l)
 
 # Output the file count
-echo "$FILE_COUNT"
+echo "$FILE_COUNT">> $GITHUB_ENV
